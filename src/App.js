22d6acd9025dcd6d2a33d5078b18f8817ec9803d
Dharisini <dharisini@besquare.com.my>
2022-07-19 14:00:22 +0800

    adding page component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Body from "./components/Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Page from "./page/Page";
 
 function App() {
   return (
     <div className="App">
       <Body />
     </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/manga/:id" element={<Page />} />
+    //     <Route path="/" element={<Body />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
