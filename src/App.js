748869ee74e73f37ff4aff725a2a9f4e019d6327
Isaiah Tan <xing.wei@besquare.com.my>
2022-07-20 12:26:56 +0800

    Added profile details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
-import Body from "./components/Body";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Page from "./page/Page";
-=======
+import Page from "./components/page/page.js";
 import Body from "./components/Body/body.js";
 // import Header from "./components/Header/header.js";
->>>>>>> 8b5877128bd55784622760f60888bbbeda973a52
 
 function App() {
   return (
